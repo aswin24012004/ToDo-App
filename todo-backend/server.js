be1f3a53c841e699f7d425bc8c17b226b8401007
Aswin <aswinaswin1w3@gmail.com>
2025-06-02 18:12:02 +0530

    Added vercel.json, .env, and updated server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const todoRoutes = require('./routes/todos');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
 
-mongoose.connect('mongodb://localhost:27017/tododb')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
